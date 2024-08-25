f8d940451e8717fd283f1d812e2a8e97bacf6806
Nikita Pedorich <pedorich-n@users.noreply.github.com>
2024-08-25 23:48:43 +0900

    Allow external configuration (#18)
    - Improve configurability
    - Improve justfile

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
+    extra-config = {
+      url = "path:./extra-config/config.nix";
+      flake = false;
+    };
+
     # Dev tools
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
@@ -21,10 +26,13 @@
   };
 
   outputs = inputs@{ flake-parts, systems, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    debug = true;
+
     systems = import systems;
     imports = [
       inputs.treefmt-nix.flakeModule
       inputs.pre-commit-hooks.flakeModule
+      inputs.extra-config.outPath
     ];
 
     perSystem = { config, lib, ... }: {
@@ -33,29 +41,42 @@
       };
 
       treefmt.config = {
-        projectRootFile = ".root";
-        flakeCheck = false;
+        projectRootFile = lib.mkDefault ".root";
+        flakeCheck = lib.mkDefault false;
 
         programs = {
           # Nix
-          nixpkgs-fmt.enable = true;
+          nixpkgs-fmt.enable = lib.mkDefault true;
+          deadnix.enable = lib.mkDefault true;
+          statix = {
+            enable = lib.mkDefault true;
+            disabled-lints = lib.mkDefault [
+              # Replaces stuff like `map (x: double x) [ 1 2 3 ]` with `map double [ 1 2 3 ]`. I don't particularly like it. IMO explicit is better.
+              "eta_reduction"
+              # Replaces stuff like `mtl = pkgs.haskellPackages.mtl;` with `inherit (pkgs.haskellPackages) mtl;`. I find this sometimes confusing. IMO explicit is better.
+              "manual_inherit_from"
+            ];
+          };
+
+          # Just
+          just.enable = lib.mkDefault true;
+
+          # Shell
+          shfmt.enable = lib.mkDefault true;
 
           # Other
-          prettier.enable = true;
+          prettier.enable = lib.mkDefault true;
         };
       };
 
       pre-commit.settings = {
-        rootSrc = lib.mkForce ../.;
+        # This value is already set in pre-commit module. Default prioriy is 100. lib.mkForce sets priority to 50.
+        # So I need to use a value that's lover than default, but higher than 50, so that downstram lib.mkForce could be used if needed
+        rootSrc = lib.mkOverride 95 ../.;
 
-        hooks = {
-          deadnix.enable = true;
-          statix.enable = true;
-
-          treefmt = {
-            enable = true;
-            package = config.treefmt.build.wrapper;
-          };
+        hooks.treefmt = {
+          enable = lib.mkDefault true;
+          package = lib.mkDefault config.treefmt.build.wrapper;
         };
       };
     };
