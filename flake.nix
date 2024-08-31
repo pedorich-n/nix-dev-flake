3fb1e66b7c7129084be813082527c09c4e2a5d6e
Nikita Pedorich <pedorich.n@gmail.com>
2024-08-31 21:18:18 +0900

    Bring back systems

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
+    systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     # Dev tools
