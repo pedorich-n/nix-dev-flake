79197145fbe8b0f3d2fbf44645cf34127e716392
Nikita Pedorich <pedorich.n@gmail.com>
2026-01-31 23:38:02 +0900

    Switch to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
